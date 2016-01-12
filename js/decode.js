027d55b670ffff5e04557c74f99ede46adf6bbe0
jescobedo <jescobedo@rmn.com>
2016-01-12 17:01:13 -0600

    Extractors

--- js/decode.js
+++ js/decode.js
@@ -8,9 +8,10 @@ function decode () {
 
 // Beautifies JSON
 function beautify () {
-	var json_object = JSON.parse(textBox.value);
+	var json_object = JSON.parse(textBox.value) || "{}";
 	var pretty = JSON.stringify(json_object, 1, '  ');
 	textBox.value = pretty;
+
 }
 
 // Parses the name-value pairs in a URL and formats them into JIRA markdown
@@ -25,14 +26,25 @@ function tablefy () {
 	textBox.value = string;
 }
 
-function unescape_text () {
+function unescapeText () {
 	var text = textBox.value.replace(/\\"/g, '"');
 	textBox.value = text;
+}
+
+function extractPayload () {
+	var obj = jQuery.parseJSON(textBox.value);
+	textBox.value = obj.query.payload;
+}
+
+function extractJ () {
+	var obj = jQuery.parseJSON(textBox.value);
+	textBox.value = obj.j;
 }
 
 
 // listeners
 var textBox = document.getElementById("textBox");
+var textBox2 = document.getElementById("textBox2");
 
 var el = document.getElementById("decode");
 el.addEventListener("click", decode);
@@ -44,5 +56,11 @@ var el = document.getElementById("beautify");
 el.addEventListener("click", beautify);
 
 var el = document.getElementById("unescape");
-el.addEventListener("click", unescape_text);
+el.addEventListener("click", unescapeText);
+
+var el = document.getElementById("extractPayload");
+el.addEventListener("click", extractPayload)
+
+var el = document.getElementById("extractJ");
+el.addEventListener("click", extractJ)
 
