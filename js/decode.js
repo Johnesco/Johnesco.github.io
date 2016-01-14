1e2448ada0b269c918a68ddf8f2532ffe888592e
John E <letmeshowyou@gmail.com>
2016-01-14 00:35:10 -0600

    do I really need that extra var?

--- js/decode.js
+++ js/decode.js
@@ -1,52 +1,50 @@
+"use strict";
+
 // Functions
 
 // Decodes a URL
-function decode () {
-	var uri = textBox.value;
-	textBox.value = decodeURIComponent(uri);
+function decode() {
+  var uri = textBox.value;
+  textBox.value = decodeURIComponent(uri);
 }
 
 // Beautifies JSON
-function beautify () {
+function beautify() {
 	var json_object = JSON.parse(textBox.value) || "{}";
-	var pretty = JSON.stringify(json_object, 1, '  ');
-	textBox.value = pretty;
-
+	textBox.value = JSON.stringify(json_object, 1, '  ');
 }
 
 // Parses the name-value pairs in a URL and formats them into JIRA markdown
-function tablefy () {
-	var string = "";
-	var uri = URI.parseQuery(textBox.value);
-		for (var key in uri) {
-  		if (uri.hasOwnProperty(key)) {
-    		string+=("|*" + key + ":* | " + uri[key] + " | \n");
-  		}
-	}
-	textBox.value = string;
-}
+function tablefy() {
+  var string = "";
+  var uri = URI.parseQuery(textBox.value);
+    for (var key in uri) {
+      if (uri.hasOwnProperty(key)) {
+        string+=("|*" + key + ":* | " + uri[key] + " | \n");
+      }
+    }
+  textBox.value = string;
+};
 
-function unescapeText () {
+function unescapeText() {
 	var text = textBox.value.replace(/\\"/g, '"');
 	textBox.value = text;
 }
 
-function extractPayload () {
+function extractPayload() {
 	var obj = jQuery.parseJSON(textBox.value);
 	textBox.value = obj.query.payload;
 }
 
-function extractJ () {
+function extractJ() {
 	var obj = jQuery.parseJSON(textBox.value);
 	textBox.value = obj.j;
 }
 
-function extractRequestBody (target) {
+function extractRequestBody() {
 	var obj = jQuery.parseJSON(textBox.value);
 	textBox.value = obj.metadata.requestBody;
 }
-
-
 
 
 // listeners
@@ -71,8 +69,11 @@ var el = document.getElementById("extractPayload");
 el.addEventListener("click", extractPayload)
 
 var el = document.getElementById("extractJ");
-el.addEventListener("click", extractJ)
+el.addEventListener("click", extractJ);
 
 var el = document.getElementById("extractRequestBody");
-el.addEventListener("click", extractRequestBody)
+el.addEventListener("click", extractRequestBody);
+
+
+
 
