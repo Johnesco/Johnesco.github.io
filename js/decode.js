e733a23dca99febfe40dc44c3b8830f121ed1f26
jescobedo <jescobedo@rmn.com>
2016-03-08 10:50:36 -0600

    New button - Expand all

--- js/decode.js
+++ js/decode.js
@@ -5,8 +5,8 @@ var textBox = $('#textBox');
 
 // Beautify JSON in TextBox
 function beautify() {
-    var json_object = JSON.parse( textBox.val() ) || '{"none":"none"}';
-    textBox.val( JSON.stringify( json_object, 1, '  ' ));
+    var json_object = JSON.parse(textBox.val()) || '{"none":"none"}';
+    textBox.val(JSON.stringify(json_object, 1, '  '));
 }
 
 // Display JSON Tree
@@ -17,7 +17,9 @@ function viewJsonTree() {
 
 // Button press event
 function modifyTextbox(fun) {
-    if (fun) textBox.val(fun(textBox.val())); // modifies text box via function
+    if (fun) {
+        textBox.val(fun(textBox.val())); // modifies text box via function
+    }
     beautify();
     viewJsonTree();
 }
@@ -25,46 +27,55 @@ function modifyTextbox(fun) {
 // Functions Used By modifyTextbox
 
 // Decode URL encoded text
-var decodeFun = function(text) {
+var decodeFun = function (text) {
     return decodeURIComponent(text);
-}
+};
 
 // Extract "j:" portion of JSON
-var extractJFun = function(text) {
+var extractJFun = function (text) {
 	return ($.parseJSON(text).j || text); // if no j:, return self
-}
+};
+
+// Extract all and piece together
+var extractAllFun = function (text) {
+    text = decodeURIComponent(text); // decode text
+    var JSON_object = $.parseJSON(text); // object
+    var jString = $.parseJSON(text).j; // string from j: (unescaped)
+    JSON_object.j = $.parseJSON(jString); // covert then insert j
+    return JSON.stringify(JSON_object); // return string
+
+};
 
 // Both Decode URL encoded text and Extract "j:" portion of JSON
-var decodeExtractJFun = function(text) {
+var decodeExtractJFun = function (text) {
     return extractJFun(decodeFun(text));
-}
+    //return (string.replace( '"p":', '"platform":' ));
+};
 
 // Extract query.payload portion of JSON
 var extractPayloadFun = function (text) {
 	return ($.parseJSON(text).query.payload);
-}
+};
 
 // Extract metadata.requestBody portion of JSON then decode it
 var extractRequestBodyFun = function (text) {
 	var coded = ($.parseJSON(text).metadata.requestBody);
     return decodeFun(coded);
-}
+};
 
 // Parses the name-value pairs in a URL and formats them into JIRA markdown
 var tablefyFun = function (text) {
-    if (text.includes('http://b.rmntest.com/'))
-        {
-           var uri = URI.parseQuery(text);
-	       var string = "";
-	       for (var key in uri) {
-		      if ( uri.hasOwnProperty( key )) {
+    if (text.includes('http://b.rmntest.com/')) {
+        var uri = URI.parseQuery(text), string = "";
+        for (var key in uri) {
+                   if ( uri.hasOwnProperty( key )) {
 			     string+=( "|*" + key + ":* | " + uri[key] + " | \n" );
 		      }
 	       }
 	       return (string.replace( 'http://b.rmntest.com/', '' )); 
         }
     else return text;
-};
+} ;
 
 
 // jQuery Event Listener
@@ -72,6 +83,7 @@ var tablefyFun = function (text) {
 $('#decodeJbeautify').on('click', function() {modifyTextbox(decodeExtractJFun);});
 $('#decode').on('click', function() {modifyTextbox(decodeFun);});
 $('#extractJ').on('click', function() {modifyTextbox(extractJFun);});
+$('#extractAll').on('click', function() {modifyTextbox(extractAllFun);});
 $('#extractPayload').on('click', function() {modifyTextbox(extractPayloadFun);});
 $('#extractRequestBody').on('click', function() {modifyTextbox(extractRequestBodyFun);});
 $('#tablefy').on('click', function() {modifyTextbox(tablefyFun);});
