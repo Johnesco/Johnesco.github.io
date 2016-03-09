660e53898a957508840116d9ffad0fa19abfb6e6
jescobedo <jescobedo@rmn.com>
2016-03-09 17:11:12 -0600

    Clean up + beautify on mouseout, removed buttons

--- js/decode.js
+++ js/decode.js
@@ -24,19 +24,19 @@ function modifyTextbox(fun) {
     beautify();
     viewJsonTree();
     showKibanaLink();
+    
 }
 
 function showKibanaLink() {
-    if (textBox.val().includes('"i":')){
+    if (textBox.val().includes('"i": ')){
         var KibanaUUID = ($.parseJSON(textBox.val()).j.e.i);
-        console.log(KibanaUUID);
-        var string = "<a target=\"_blank\" href=\"http://analytics-test.wsmeco.com/_plugin/kibana/#/discover?_g=(refreshInterval:(display:Off,section:0,value:0),time:(from:now%2Fd,mode:quick,to:now%2Fd))&_a=(columns:!(_source),index:analytics,interval:auto,query:(query_string:(analyze_wildcard:!t,query:'"
+        var string = "Kibana Link: <a target=\"_blank\" href=\"http://analytics-test.wsmeco.com/_plugin/kibana/#/discover?_g=(refreshInterval:(display:Off,section:0,value:0),time:(from:now%2Fd,mode:quick,to:now%2Fd))&_a=(columns:!(_source),index:analytics,interval:auto,query:(query_string:(analyze_wildcard:!t,query:'"
                     + KibanaUUID
                     + "')),sort:!(owen.event.eventTimestamp,desc))\">"
                     + KibanaUUID
                     + "</a>"
-        $(kibanaLink).append(string);
-    } else { kibanaLink.text(""); }   
+        $(kibanaLink).html(string);
+    } else { kibanaLink.text("Kibana Link: (none)"); }   
 }
 
 // Functions Used By modifyTextbox
@@ -46,10 +46,7 @@ var decodeFun = function (text) {
     return decodeURIComponent(text);
 };
 
-// Extract "j:" portion of JSON
-var extractJFun = function (text) {
-	return ($.parseJSON(text).j || text); // if no j:, return self
-};
+
 
 // Extract all and piece together
 var extractAllFun = function (text) {
@@ -61,10 +58,12 @@ var extractAllFun = function (text) {
 
 };
 
-// Both Decode URL encoded text and Extract "j:" portion of JSON
-var decodeExtractJFun = function (text) {
-    return extractJFun(decodeFun(text));
-    //return (string.replace( '"p":', '"platform":' ));
+// Extract j: and show alone
+var extractJFun = function (text) {
+    var JSON_object = $.parseJSON(text); // object
+    var jString = $.parseJSON(text).j; // string from j: (unescaped)
+    JSON_object.j = $.parseJSON(jString); // covert then insert j
+    return JSON.stringify(JSON_object); // return string
 };
 
 // Extract query.payload portion of JSON
@@ -95,14 +94,16 @@ var tablefyFun = function (text) {
 
 // jQuery Event Listener
 // http://api.jquery.com/on/
-$('#decodeJbeautify').on('click', function() {modifyTextbox(decodeExtractJFun);});
-$('#decode').on('click', function() {modifyTextbox(decodeFun);});
-$('#extractJ').on('click', function() {modifyTextbox(extractJFun);});
 $('#extractAll').on('click', function() {modifyTextbox(extractAllFun);});
+$('#extractJ').on('click', function() {modifyTextbox(extractJFun);});
+$('#decode').on('click', function() {modifyTextbox(decodeFun);});
 $('#extractPayload').on('click', function() {modifyTextbox(extractPayloadFun);});
 $('#extractRequestBody').on('click', function() {modifyTextbox(extractRequestBodyFun);});
 $('#tablefy').on('click', function() {modifyTextbox(tablefyFun);});
 $('#beautifyTree').on('click', function() {modifyTextbox();});
+
+$('#textBox').on('mouseout', function() {modifyTextbox();});
+
 
 
 
