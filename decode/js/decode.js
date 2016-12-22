86e97cea33c0f568c5069ab8c64ffd6d84ca44f7
John Escobedo <jescobedo@rmn.com>
2016-12-22 13:44:45 -0600

    refactor decoder onclick

--- decode/js/decode.js
+++ decode/js/decode.js
@@ -16,6 +16,7 @@ function viewJsonTree() {
   $('#jjson').jJsonViewer(jjson);
 }
 
+// Search for "i:" parameter and make link to Kibana
 function showKibanaLink() {
     if (textBox.val().includes('"i": ')){
         var KibanaUUID = ($.parseJSON(textBox.val()).j.e.i);
@@ -34,22 +35,16 @@ function showKibanaLink() {
     } else { kibanaLink.text("Kibana Link: (none)"); }
 }
 
-// This function is run from each button press, uses the functions below to process text.
-function modifyTextbox(fun) {
-    if (fun) {
-        textBox.val(fun(textBox.val())); // modifies text box via function
-    }
+// This function optionally recieves a function from the event
+function modifyTextbox(event) {
+    if (event.data) {
+        textBox.val(event.data.function(textBox.val())); // modifies text box via function
+    } 
     beautify();
     viewJsonTree();
 }
 
 // Functions Used By modifyTextbox
-
-// Decode URL encoded text
-var decodeFun = function (text) {
-    text = text.replace(/\+/g, ' ');
-    return decodeURIComponent(text);
-};
 
 // Extract all and piece together
 var extractAllFun = function (text) {
@@ -60,38 +55,18 @@ var extractAllFun = function (text) {
     return JSON.stringify(JSON_object); // return string
 };
 
-// Extract j: and show alone (Deprecated, will be removed)
-var extractJFun = function (text) {
-    var JSON_object = $.parseJSON(text); // object
-    var jString = $.parseJSON(text).j; // string from j: (unescaped)
-    JSON_object.j = $.parseJSON(jString); // covert then insert j
-    return JSON.stringify(JSON_object); // return string
+// Decode URL encoded text
+var decodeFun = function (text) {
+    text = text.replace(/\+/g, ' ');
+    return decodeURIComponent(text);
 };
 
-// Extract query.payload portion of JSON
-var extractPayloadFun = function (text) {
-  return ($.parseJSON(text).query.payload);
-};
+// decodes URL and populates table
+var magicFormat = function (text) {
+    text = decodeFun(text);
+    text = stripKeysAndInsertIntoTable(text);
 
-// Extract metadata.requestBody portion of JSON then decode it
-var extractRequestBodyFun = function (text) {
-  var coded = ($.parseJSON(text).metadata.requestBody);
-    return decodeFun(coded);
-};
-
-// Parses the name-value pairs in a URL and formats them into JIRA markdown
-var tablefyFun = function (text) {
-    if (text.includes('/b.rmntest.com/')) {
-        var uri = URI.parseQuery(text);
-        var string = "";
-        for (var key in uri) {
-                   if ( uri.hasOwnProperty( key )) {
-           string+=( "|*" + key + ":* | " + uri[key] + " | \n" );
-          }
-         }
-         return (string.replace( 'http://b.rmntest.com/', '' ));
-        }
-    else return text;
+    return text;
 };
 
 var stripKeysAndInsertIntoTable = function (text) {
@@ -110,27 +85,40 @@ var stripKeysAndInsertIntoTable = function (text) {
     return text;
 };
 
-var magicFormat = function (text) {
-    text = decodeFun(text);
-    text = stripKeysAndInsertIntoTable(text);
-
-    return text;
+// Parses the name-value pairs in a URL and formats them into JIRA markdown
+var tablefyFun = function (text) {
+    if (text.includes('/b.rmntest.com/')) {
+        var uri = URI.parseQuery(text);
+        var string = "";
+        for (var key in uri) {
+                   if ( uri.hasOwnProperty( key )) {
+           string+=( "|*" + key + ":* | " + uri[key] + " | \n" );
+          }
+         }
+         return (string.replace( 'http://b.rmntest.com/', '' ));
+        }
+    else return text;
 };
+
+
+
 
 // jQuery Event Listener
 // http://api.jquery.com/on/
 
-$('#extractAll').on('click', modifyTextbox.bind(null, extractAllFun));
-
-// $('#extractAll').on('click', function () {
-//     modifyTextbox(extractAllFun);
-// });
-// Deprecated: $('#extractJ').on('click', function() {modifyTextbox(extractJFun);});
-$('#decode').on('click', modifyTextbox.bind(null, decodeFun));
-$('#extractPayload').on('click', modifyTextbox.bind(null, extractPayloadFun));
-$('#extractRequestBody').on('click', modifyTextbox.bind(null, extractRequestBodyFun));
-$('#tablefy').on('click', modifyTextbox.bind(null, tablefyFun));
-$('#beautifyTree').on('click', modifyTextbox.bind(null, null));
-$('#textBox').on('mouseout', modifyTextbox.bind(null, null));
-$('#magicFormat').on('click', modifyTextbox.bind(null, magicFormat));
+$('#extractAll').on('click', {function : extractAllFun}, modifyTextbox);
+$('#decode').on('click', {function : decodeFun}, modifyTextbox);
+$('#tablefy').on('click', {function : tablefyFun}, modifyTextbox);
+$('#magicFormat').on('click', {function:magicFormat}, modifyTextbox);
+$('#beautifyTree').on('click', modifyTextbox);
+$('#textBox').on('mouseout', modifyTextbox);
 $('#showKibanaLink').on('click', showKibanaLink);
+
+// alternate method with bind
+// $('#extractAll').on('click', modifyTextbox.bind(null, extractAllFun));
+// $('#decode').on('click', modifyTextbox.bind(null, decodeFun));
+// $('#tablefy').on('click', modifyTextbox.bind(null, tablefyFun));
+// $('#beautifyTree').on('click', modifyTextbox);
+// $('#textBox').on('mouseout', modifyTextbox.bind(null, null));
+// $('#magicFormat').on('click', modifyTextbox.bind(null, magicFormat));
+// $('#showKibanaLink').on('click', showKibanaLink);
