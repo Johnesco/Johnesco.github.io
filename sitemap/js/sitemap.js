f4d1491f8f3a4dbbbfd28dd708187e0914c64371
John Escobedo <jescobedo@rmn.com>
2017-01-18 15:55:39 -0600

    rearrange functions

--- sitemap/js/sitemap.js
+++ sitemap/js/sitemap.js
@@ -1,10 +1,9 @@
 var $content = $('#content'); 
-// var $json = $('#json'); 
 
 // Default Environment Variables
 var slice = "control";
-var env = "rmntest.com/";
-var security = "https://";
+var enviroment = "";
+var security = "";
 
 // Main object that will hold a compressed set of links
 var contentObject = {};
@@ -17,70 +16,58 @@ var contentObject = {};
   }
 }(pageList));
 
+// Real time check of values
 function getSlice(){ return $('#slice').val() || slice;}
 function getEnv(){ return $('input[name="environment"]:checked').val();}
 function getSecurity(){ return $('input[name="http"]:checked').val();}
 
-// Creates HTML for a Header Section
-function makeHeader(str){
-  return "<h2>" + str + " (slice: " + getSlice() + ")" + "</h2>";
+// Fires off update() anytime the form changes
+function update(){
+    // Cache Form Values
+    slice = getSlice();
+    environment = getEnv();
+    security = getSecurity();
+
+    $content.html(renderContent(contentObject));
 }
 
-// Creates HTML link with ?refresh and &slice from a URL
-// If nobr is false or absent, a <br> is added after link
-// If nobr is true, no <br> is added after link
-function makeAnchor(url,label){
-  // If no label is provided, use URL as label
-  var label = label || url;
+// Renders sections of object into HTML
+function renderContent(obj){
+  var finalStr ='';
 
-  // Combine abel and url into an achor tag
-  var url = 
-    '<a target="_blank" href="' + url + '?refresh=1' + '&slice=' + getSlice() + '">' +
-    label + '</a>';
+  for (section in obj){
+    var scope = obj[section]['scope'];
 
-  return url;
+    // only render sections in that match scope
+    if (inScope(scope)){
+      finalStr += renderSection(obj[section],section);
+    }
+  }
+
+  return finalStr;
 }
 
-function buildURL(pre, sub, endpoint){
-  return getSecurity() + pre + getEnv() + sub + endpoint;
+// Returns true if scope matches current environement
+function inScope(scope){
+  //var environment = getEnv();
+
+  if (!scope) { return true }
+
+  if (environment.includes(scope)) { return true } 
+
+  return false;
 }
 
-// Recieves array of endPoints
-// Returns link to store + landing pages
-function renderStoreLink(endpoint,section){
-  var StoreLink = '';
-  var pre = section['pre'];
-  var sub = section['sub'];
-
-  var url = buildURL(pre,sub,endpoint);
-  StoreLink += (makeAnchor(url)+" - ");
-
-  url = buildURL(pre,"landing/",endpoint);
-  StoreLink += (makeAnchor(url,"landing/")+" - ");
-
-  url = buildURL(pre,"landing2/",endpoint);
-  StoreLink += (makeAnchor(url,"landing2/")+" - ");
-
-  url = buildURL(pre,"landing5/",endpoint);
-  StoreLink += (makeAnchor(url,"landing5/")+"<br>");
-
-  return StoreLink;
-}
-
-function renderLink(endpoint,section){
-  var Link = '';
-  var url = buildURL(section['pre'],section['sub'],endpoint);
-  Link += (makeAnchor(url)+"<br>");
-  return Link;
-}
-
+// Recieves single section object and its name
+// Creates the header and links for that section
+// If it is a store page, landing - landing 5 links are added
+// returns it as HTML string
 function renderSection(section, sectionName){
-  // recieves single section object and returns it as HTML
-  var linkList = [];
 
   // endpoints in a section dictate the number of links to be made
   var endPoints = section['endPoints'];
   var sub = section['sub'];
+  var linkList = [];
 
   if (sub == "view/"){
     // Special Case for Store Pages
@@ -107,34 +94,58 @@ function renderSection(section, sectionName){
   return section //string
 }
 
-function inScope(scope){
-  var environment = getEnv();
+// Recieves array of endPoints
+// Returns link to store + landing pages
+function renderStoreLink(endpoint,section){
 
-  if (!scope) { return true }
+  var pre = section['pre'];
+  var sub = section['sub'];
+  var StoreLink = '';
 
-  if (environment.includes(scope)) { return true } 
+  var url = buildURL(pre,sub,endpoint);
+  StoreLink += (makeAnchor(url)+" - ");
 
-  return false;
+  url = buildURL(pre,"landing/",endpoint);
+  StoreLink += (makeAnchor(url,"landing/")+" - ");
+
+  url = buildURL(pre,"landing2/",endpoint);
+  StoreLink += (makeAnchor(url,"landing2/")+" - ");
+
+  url = buildURL(pre,"landing5/",endpoint);
+  StoreLink += (makeAnchor(url,"landing5/")+"<br>");
+
+  return StoreLink;
 }
 
-// Renders sections of object into HTML
-function renderContent(obj){
-  var finalStr ='';
-
-  for (section in obj){
-    var scope = obj[section]['scope'];
-    // only render those in scope
-    if (inScope(scope)){
-      finalStr += renderSection(obj[section],section);
-    }
-  }
-
-  return finalStr;
+function renderLink(endpoint,section){
+  var Link = '';
+  var url = buildURL(section['pre'],section['sub'],endpoint);
+  Link += (makeAnchor(url)+"<br>");
+  return Link;
 }
 
-// Fires off update from form clicks and updates
-function update(){
-    $content.html(renderContent(contentObject));
+// Creates HTML for a Header Section
+function makeHeader(str){
+  return "<h2>" + str + " (slice: " + slice + ")" + "</h2>";
+}
+
+function buildURL(pre, sub, endpoint){
+  return security + pre + environment + sub + endpoint;
+}
+
+// Creates HTML link with ?refresh and &slice from a URL
+// If nobr is false or absent, a <br> is added after link
+// If nobr is true, no <br> is added after link
+function makeAnchor(url,label){
+  // If no label is provided, use URL as label
+  var label = label || url;
+
+  // Combine abel and url into an achor tag
+  var url = 
+    '<a target="_blank" href="' + url + '?refresh=1' + '&slice=' + slice + '">' +
+    label + '</a>';
+
+  return url;
 }
 
 // Run for the first time to initialize page
