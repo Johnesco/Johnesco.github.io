90a62fc98032aafe84d4108c84cc4aed8ee85ebc
John Escobedo <letmeshowyou@gmail.com>
2019-04-17 18:52:54 -0500

    Cleaned up js

--- blackout/js/app.js
+++ blackout/js/app.js
@@ -1,12 +1,12 @@
 
 var story = "I had scarcely thought of the theater for some years, when Kean arrived in this country; and it was more from curiosity than from any other motive, that I went to see, for the first time, the great actor of the age. I was soon lost to the recollection of being in a theater, or looking upon a great display of the \"mimic art.\" The simplicity, earnestness, and sincerity of his acting made me forgetful of the fiction, and bore me away with the power of reality and truth. If this be acting, said I, as I returned home, I may as well make the theater my school, and henceforward study nature at second hand. \nHow can I describe one who is almost as full of beauties as nature itself,—who grows upon us the more we become acquainted with him, and makes us sensible that the first time we saw him in any part, however much he may have moved us, we had but a partial apprehension of the many excellences of his acting? We cease to consider it as a mere amusement. It is an intellectual feast; and he who goes to it with a disposition and capacity to relish it, will receive from it more nourishment for his mind, than he would be likely to do in many other ways in twice the time. Our faculties are opened and enlivened by it; our reflections and recollections are of an elevated kind; and the voice which is sounding in our ears, long after we have left him, creates an inward harmony which is for our good. \nKean, in truth, stands very much in that relation to other players whom we have seen, that Shakspeare does to other dramatists. One player is called classical; another makes fine points here, and another there; Kean makes more fine points than all of them together; but in him these are only little prominences, showing their bright heads above a beautifully undulated surface. A continual change is going on in him, partaking of the nature of the varying scenes he is passing through, and the many thoughts and feelings which are shifting within him.";
 
-//var penState = "off";
+// VARIABLES
 var blockedOut = false;
 var faded = false;
 var punctuation = /([—.,?!;:"'])/;
 
-
+// FUNCTIONS
 function displayStory(storytext) {
   var poem = '';
   var array = storytext.split(' ');
@@ -14,6 +14,23 @@ function displayStory(storytext) {
     poem += "<span class>" + word + " </span>" 
   });
   $(".story").html(poem);
+}
+
+
+
+
+
+// Update text of poem
+function updatePoem () {
+  $("div.poem").html($("span.marked").text());
+}
+
+
+function newStory (){
+  var storytext = $(".textarea").val()
+  $('.story').html(displayStory(storytext));
+  $(".textarea").val('');
+  return false;
 }
 
 displayStory(story);
@@ -24,6 +41,12 @@ displayStory(story);
 
 
 
+
+// CLICK RESET: Remove all classes, clear poem
+$(".reset").on('click',function(){
+$("span").removeClass('blocked faded marked');
+$(".poem").html('(Marks words above to populate your poem)');
+});
 
 
 // Clicking on Words
@@ -38,48 +61,6 @@ $("div.story").on("click",function (evt){
 });
 
 
-
-// Remove all classes, clear poem
-$(".reset").on('click',function(){
-  $("span").removeClass('blocked faded marked');
-  $(".poem").html('(Marks words above to populate your poem)');
-});
-
-
-// Update text of poem
-function updatePoem () {
-  $("div.poem").html($("span.marked").text());
-}
-
-$(".toggleFade").on('click', function(){
-  if (faded){
-    unfade();
-    faded = false;
-  } else {
-    fade();
-    faded = true;
-  }
-});
-
-function fadeUnfade(){
-  $("span.blocked").addClass('faded')
-  /*if (faded){
-    unfade();
-  } else {
-    fade();
-  }*/
-}
-
-function fade(){
-  $("span.blocked").addClass('faded')
-   faded = true;
-}
-
-function unfade(){
-  $("span").removeClass('faded');
-  faded = false;
-}
-
 // Block out all words not "marked"
 $(".blockOut").on('click', function(){
   if(blockedOut){
@@ -93,44 +74,9 @@ $(".blockOut").on('click', function(){
 
 // 
 $(".clearMarked").on('click', function(){
-  
-   $("span").removeClass("marked");
+  $("span").removeClass("marked");
+  $(".poem").html('(Marks words above to populate your poem)');
 });
 
-function newStory (){
-  var storytext = $(".textarea").val()
-  $('.story').html(displayStory(storytext));
-  $(".textarea").val('');
-  return false;
-}
 
 
-/*  Mouseover Behavior
-$("div.story").on("mouseover",function (evt){
-  
-  if (this === evt.target || $(evt.target).hasClass("marked")){
-    // do nothing
-  } else {
-    switch (penState){
-      case "blocks":
-        $(evt.target).addClass("blocked");
-        break;
-      case "clears":
-        $(evt.target).removeClass("blocked");
-        break;
-    }
-  }
-  //updatePoem();
-});
-
-//Button, toggle mouseover behavior
-$(".penState").on('click',function(){
-  if ( penState === 'off' ){
-    penState = 'blocks';
-  } else if ( penState === 'blocks' ){
-    penState = 'clears';  
-  } else {
-    penState = 'off';
-  }
-  $('.penState').text("Mouseover: " + penState);
-});*/
