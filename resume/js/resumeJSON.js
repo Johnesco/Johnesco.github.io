1f6325589a8a39ce2ca281bd8ff8bea92332b05f
John <letmeshowyou@gmail.com>
2019-07-31 16:51:01 -0500

    comma 2

--- resume/js/resumeJSON.js
+++ resume/js/resumeJSON.js
@@ -32,7 +32,7 @@ var resumeJSON = {
             "website": "",
             "startDate": "March 2019",
             "endDate": "Present",
-            "summary": "Test Engineer contractor. Ensured Requirement Tracablity Matrix was intact and ran regression on existing features. Developed and updated test cases in IBM Rational": [
+            "summary": "Test Engineer contractor. Ensured Requirement Tracablity Matrix was intact and ran regression on existing features. Developed and updated test cases in IBM Rational"
             "highlights": [
               "Enforced protocol and patterns for reproducable testing.",
               "Created and updated test cases and test scripts.", 
@@ -46,7 +46,7 @@ var resumeJSON = {
             "website": "https://youscience.com",
             "startDate": "June 2018",
             "endDate": "October 2018",
-            "summary": "Integrated QA process into their software development cycle while working towards a CI/CD pipeline. Tested buids before shipping. Managed risk in releasing new features": [
+            "summary": "Integrated QA process into their software development cycle while working towards a CI/CD pipeline. Tested buids before shipping. Managed risk in releasing new features.",
             "highlights": [
             "Developed and maintained QA pipeline for CI/CD.",
               "Tested incoming features for website.", 
