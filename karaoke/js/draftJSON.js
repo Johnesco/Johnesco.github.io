9d8a934de7f7e2969a5b45b37ca81e383cbb6be5
John <letmeshowyou@gmail.com>
2025-04-12 18:49:33 -0500

    initial karaoke code

---
+++ karaoke/js/draftJSON.js
@@ -0,0 +1,38 @@
+{
+  "listings": [
+    {
+    "Venue Name": "Common Interest",
+    "Address:" : [{
+      "Address" : "123",
+      "Street" : "Lamar",
+      "State" : "Texas",
+      "zip" : "78745"
+    }],
+    "KJ": [{
+      "Host":"Various",
+      "Company":""
+    }],
+    "schedule": {
+      "weekly": ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+      "ordinal": [["first", "Thursday"],["third", "Tuesday"]]
+    }
+    },
+    {
+    "Venue Name": "Cheerz",
+      "Address:" : [{
+      "Address" : "123",
+      "Street" : "Lamar",
+      "State" : "Texas",
+      "zip" : "78745"
+    }],
+    "KJ": [{
+      "Host":"Various",
+      "Company":""
+    }],
+    "schedule": {
+      "weekly": ["Friday", "Saturday"],
+      "ordinal": [["first", "Thursday"],["third", "Tuesday"]]
+    }
+    }
+]
+}  
