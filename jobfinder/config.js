2da78b04bac19f9ad5c29fd186e09c87ecb5f9ce
John <letmeshowyou@gmail.com>
2026-01-26 08:46:00 -0600

    job finder app updated

--- jobfinder/config.js
+++ jobfinder/config.js
@@ -1,54 +1,157 @@
 // JobFinder Configuration
 // Job boards organized by category
+// URL placeholders: {keyword}, {city}, {state}
 
 const CONFIG = {
   categories: [
     {
       name: "General",
       sites: [
-        { name: "LinkedIn", url: "https://www.linkedin.com/jobs/search/?keywords={keyword}" },
-        { name: "Indeed", url: "https://www.indeed.com/jobs?q={keyword}" },
-        { name: "Glassdoor", url: "https://www.glassdoor.com/Job/jobs.htm?sc.keyword={keyword}" },
-        { name: "ZipRecruiter", url: "https://www.ziprecruiter.com/jobs-search?search={keyword}" },
-        { name: "Monster", url: "https://www.monster.com/jobs/search?q={keyword}" },
-        { name: "CareerBuilder", url: "https://www.careerbuilder.com/jobs?keywords={keyword}" },
-        { name: "SimplyHired", url: "https://www.simplyhired.com/search?q={keyword}" },
-        { name: "Google Jobs", url: "https://www.google.com/search?q={keyword}+jobs&ibp=htl;jobs" },
-        { name: "USAJobs", url: "https://www.usajobs.gov/Search/Results?k={keyword}" },
-        { name: "Snagajob", url: "https://www.snagajob.com/search?w={keyword}" }
+        {
+          name: "LinkedIn",
+          urls: {
+            keyword: "https://www.linkedin.com/jobs/search/?keywords={keyword}",
+            location: "https://www.linkedin.com/jobs/search/?keywords={keyword}&location={city}%2C%20{state}",
+            remote: "https://www.linkedin.com/jobs/search/?keywords={keyword}&f_WT=2"
+          }
+        },
+        {
+          name: "Indeed",
+          urls: {
+            keyword: "https://www.indeed.com/jobs?q={keyword}",
+            location: "https://www.indeed.com/jobs?q={keyword}&l={city}%2C%20{state}",
+            remote: "https://www.indeed.com/jobs?q={keyword}&l=remote"
+          }
+        },
+        {
+          name: "Glassdoor",
+          urls: {
+            keyword: "https://www.glassdoor.com/Job/jobs.htm?sc.keyword={keyword}",
+            location: "https://www.glassdoor.com/Job/jobs.htm?sc.keyword={keyword}&locT=C&locKeyword={city}%2C%20{state}",
+            remote: "https://www.glassdoor.com/Job/jobs.htm?sc.keyword={keyword}&remoteWorkType=1"
+          }
+        },
+        {
+          name: "ZipRecruiter",
+          urls: {
+            keyword: "https://www.ziprecruiter.com/jobs-search?search={keyword}",
+            location: "https://www.ziprecruiter.com/jobs-search?search={keyword}&location={city}%2C%20{state}",
+            remote: "https://www.ziprecruiter.com/jobs-search?search={keyword}&location=Remote%20%28USA%29"
+          }
+        },
+        {
+          name: "Monster",
+          urls: {
+            keyword: "https://www.monster.com/jobs/search?q={keyword}",
+            location: "https://www.monster.com/jobs/search?q={keyword}&where={city}%2C%20{state}",
+            remote: "https://www.monster.com/jobs/search?q={keyword}&where=remote"
+          }
+        },
+        {
+          name: "CareerBuilder",
+          urls: {
+            keyword: "https://www.careerbuilder.com/jobs?keywords={keyword}",
+            location: "https://www.careerbuilder.com/jobs?keywords={keyword}&location={city}%2C%20{state}",
+            remote: "https://www.careerbuilder.com/jobs?keywords={keyword}&cb_workhome=remote"
+          }
+        },
+        {
+          name: "SimplyHired",
+          urls: {
+            keyword: "https://www.simplyhired.com/search?q={keyword}",
+            location: "https://www.simplyhired.com/search?q={keyword}&l={city}%2C%20{state}",
+            remote: "https://www.simplyhired.com/search?q={keyword}&l=remote"
+          }
+        }
       ]
     },
     {
       name: "Tech",
       sites: [
-        { name: "Dice", url: "https://www.dice.com/jobs?q={keyword}" },
-        { name: "Built In", url: "https://builtin.com/jobs?search={keyword}" },
-        { name: "Wellfound", url: "https://wellfound.com/jobs?query={keyword}" },
-        { name: "Stack Overflow", url: "https://stackoverflowjobs.com/?q={keyword}" },
-        { name: "Crunchboard", url: "https://www.crunchboard.com/jobs?q={keyword}" },
-        { name: "Authentic Jobs", url: "https://authenticjobs.com/?search={keyword}" }
+        {
+          name: "Dice",
+          urls: {
+            keyword: "https://www.dice.com/jobs?q={keyword}",
+            location: "https://www.dice.com/jobs?q={keyword}&location={city}%2C%20{state}",
+            remote: "https://www.dice.com/jobs?q={keyword}&filters.workplaceTypes=Remote&filters.isRemote=true"
+          }
+        },
+        {
+          name: "Built In",
+          urls: {
+            keyword: "https://builtin.com/jobs?search={keyword}",
+            location: "https://builtin.com/jobs/office?search={keyword}&city={city}&state={state}&country=USA&allLocations=true",
+            remote: "https://builtin.com/jobs/remote?search={keyword}&allLocations=true"
+          }
+        },
+        {
+          name: "Stack Overflow",
+          urls: {
+            keyword: "https://stackoverflowjobs.com/?q={keyword}",
+            location: "https://stackoverflowjobs.com/?q={keyword}&l={city}%2C%20{state}",
+            remote: "https://stackoverflowjobs.com/?q={keyword}&r=true"
+          }
+        },
+        {
+          name: "Crunchboard",
+          urls: {
+            keyword: "https://www.crunchboard.com/jobs/search?q={keyword}",
+            location: "https://www.crunchboard.com/jobs/search?q={keyword}&location={city}%2C%20{state}",
+            remote: "https://www.crunchboard.com/jobs/search?q={keyword}&l=Remote&remote=full"
+          }
+        }
       ]
     },
     {
       name: "Remote",
       sites: [
-        { name: "FlexJobs", url: "https://www.flexjobs.com/search?search={keyword}" },
-        { name: "Remote.co", url: "https://remote.co/remote-jobs/search/?search_keywords={keyword}" },
-        { name: "We Work Remotely", url: "https://weworkremotely.com/remote-jobs/search?term={keyword}" },
-        { name: "RemoteOK", url: "https://remoteok.com/remote-{keyword}-jobs" },
-        { name: "Working Nomads", url: "https://www.workingnomads.com/jobs?tag={keyword}" },
-        { name: "JustRemote", url: "https://justremote.co/remote-jobs?search={keyword}" },
-        { name: "Remotive", url: "https://remotive.com/remote-jobs/search?text={keyword}" }
+        {
+          name: "RemoteOK",
+          urls: {
+            keyword: "https://remoteok.com/?search={keyword}"
+          }
+        },
+        {
+          name: "Working Nomads",
+          urls: {
+            keyword: "https://www.workingnomads.com/jobs?tag={keyword}"
+          }
+        },
+        {
+          name: "JustRemote",
+          urls: {
+            keyword: "https://justremote.co/remote-jobs?search={keyword}"
+          }
+        }
       ]
     },
     {
       name: "Startup",
       sites: [
-        { name: "AngelList", url: "https://wellfound.com/jobs?query={keyword}" },
-        { name: "The Muse", url: "https://www.themuse.com/search?keyword={keyword}" },
-        { name: "Idealist", url: "https://www.idealist.org/en/jobs?q={keyword}" },
-        { name: "Y Combinator", url: "https://www.workatastartup.com/jobs?query={keyword}" },
-        { name: "VentureLoop", url: "https://www.ventureloop.com/ventureloop/job_search.php?keywords={keyword}" }
+        {
+          name: "The Muse",
+          urls: {
+            keyword: "https://www.themuse.com/search/keyword/{keyword}",
+            location: "https://www.themuse.com/search/location/{city}-{state}/keyword/{keyword}",
+            remote: "https://www.themuse.com/search/keyword/{keyword}/flexible/true"
+          }
+        },
+        {
+          name: "Idealist",
+          urls: {
+            keyword: "https://www.idealist.org/en/jobs?q={keyword}",
+            location: "https://www.idealist.org/en/jobs?q={keyword}&location={city}%2C%20{state}",
+            remote: "https://www.idealist.org/en/jobs?q={keyword}&locationType=REMOTE"
+          }
+        },
+        {
+          name: "VentureLoop",
+          urls: {
+            keyword: "https://www.ventureloop.com/ventureloop/job_search.php?g=0&kword={keyword}&jcat=%25&dc=all&ldata=%25&jt=1&d=5&btn=1",
+            location: "https://www.ventureloop.com/ventureloop/job_search.php?g=0&kword={keyword}&jcat=%25&dc=all&ldata={city},%20{state},%20US&jt=1&d=5&btn=1",
+            remote: "https://www.ventureloop.com/ventureloop/job_search.php?g=0&kword={keyword}&jcat=%25&dc=all&ldata=%25&jt=1&remote=work_remotely&d=5&btn=1"
+          }
+        }
       ]
     }
   ]
